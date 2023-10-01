b60cf2f6564c8fe486c3c3e1a2231008d8e24837
Nasuton <lovetennis_airu@yahoo.co.jp>
2023-10-01 11:33:12 +0900

    Enum Class Add

---
+++ Enum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_SampleProjects
+{
+    enum AESType
+    {
+        AES_128 = 0,
+        AES_192 = 1,
+        AES_256 = 2,
+    }
+}
